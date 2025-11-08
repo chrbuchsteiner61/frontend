30f9cc603dd0f63a51131152cce88c31b4e1dafa
chrbuchsteiner61 <chrbuchsteiner@t-onlione.de>
2025-11-08 11:30:42 +0100

    first commit

---
+++ src/app.rs
@@ -0,0 +1,11 @@
+use leptos::*;
+use leptos::tachys::html::element::ElementChild;
+
+#[component]
+pub fn App() -> impl IntoView {
+    view! { 
+        <div>
+            <h1>"Meine RUST-Versuche"</h1>
+        </div>
+    }
+}
